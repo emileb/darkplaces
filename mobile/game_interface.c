d3b33b236c64aa92640126ccf99d1c182ba429e5
Emile Belanger <emile.belanger@gmail.com>
2020-03-29 11:37:46 +0100

    Added mouse 2 as Alt Attack

--- mobile/game_interface.c
+++ mobile/game_interface.c
@@ -26,6 +26,13 @@ void PortableInit(int argc,const char ** argv)
 }
 
 
+extern void Android_OnMouse( int androidButton, int action, float x, float y);
+
+#define ACTION_DOWN 0
+#define ACTION_UP 1
+#define BUTTON_PRIMARY 1
+#define BUTTON_SECONDARY 2
+
 extern int SDL_SendKeyboardKey(Uint8 state, SDL_Scancode scancode);
 
 int PortableKeyEvent(int state, int code ,int unitcode)
@@ -128,6 +135,12 @@ void PortableAction(int state, int action)
             break;
         case PORT_ACT_DOWN:
             (state)?KeyDownPort(&in_down):KeyUpPort(&in_down);
+            break;
+        case PORT_ACT_ALT_ATTACK:
+            if ( state )
+                Android_OnMouse(BUTTON_SECONDARY, ACTION_DOWN, 0, 0);
+            else
+                Android_OnMouse(BUTTON_SECONDARY, ACTION_UP, 0, 0);
             break;
         case PORT_ACT_WEAP1:
             if ( state )
