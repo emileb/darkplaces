470b82e4d658b3e6d2a6af85ae49ef96e957570d
Emile Belanger <emile.belanger@gmail.com>
2020-11-07 17:24:39 +0000

    Moved mouse button code

--- mobile/game_interface.c
+++ mobile/game_interface.c
@@ -26,12 +26,8 @@ void PortableInit(int argc,const char ** argv)
 }
 
 
-extern void Android_OnMouse( int androidButton, int action, float x, float y);
+//extern void Android_OnMouse( int androidButton, int action, float x, float y);
 
-#define ACTION_DOWN 0
-#define ACTION_UP 1
-#define BUTTON_PRIMARY 1
-#define BUTTON_SECONDARY 2
 
 extern int SDL_SendKeyboardKey(Uint8 state, SDL_Scancode scancode);
 
@@ -137,10 +133,7 @@ void PortableAction(int state, int action)
             (state)?KeyDownPort(&in_down):KeyUpPort(&in_down);
             break;
         case PORT_ACT_ALT_ATTACK:
-            if ( state )
-                Android_OnMouse(BUTTON_SECONDARY, ACTION_DOWN, 0, 0);
-            else
-                Android_OnMouse(BUTTON_SECONDARY, ACTION_UP, 0, 0);
+        	MouseButton(state, BUTTON_SECONDARY);
             break;
         case PORT_ACT_WEAP1:
             if ( state )
@@ -218,7 +211,7 @@ void PortableAction(int state, int action)
 	}
 }
 
-char * quickCommand = 0;
+static const char * quickCommand = 0;
 void PortableCommand(const char * cmd)
 {
 	quickCommand = cmd;
