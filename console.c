b43f9b002869febc38f9b12e74e0014d7c5fd318
Emile Belanger <emile.belanger@gmail.com>
2021-08-19 17:19:18 +0100

    Added Android logging

--- console.c
+++ console.c
@@ -1147,6 +1147,10 @@ static char Sys_Con_NearestColor(const unsigned char _r, const unsigned char _g,
 		return '1';
 }
 
+#ifdef __ANDROID__
+#include "LogWritter.h"
+#endif
+
 /*
 ================
 Con_MaskPrint
@@ -1160,7 +1164,10 @@ void Con_MaskPrint(int additionalmask, const char *msg)
 	static int mask = 0;
 	static int index = 0;
 	static char line[MAX_INPUTLINE];
-
+#ifdef __ANDROID__
+    LOGI("darkplaces: %s",msg);
+    LogWritter_Write(msg);
+#endif
 	if (con_mutex)
 		Thread_LockMutex(con_mutex);
 
